75bf8592e759edd43900070dc1cb630524cee2fa
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2023-01-22 17:01:34 -0300

    A bug was fixed

--- src/regist.c
+++ src/regist.c
@@ -1,19 +1,20 @@
+#include <stdio.h>
 
+#ifndef config_h
+#include "config.h"
+#endif
 
 void
-regist_team(GROUP *cup)
+regist_team(TEAM teams)
 {
-	char group_names[AMOUNT_OF_GROUPS];
 	char group;
 
-	group_names = get_group_names(group_names);
-
 	group = get_group(group_names);
-	while (get_amount_of_registered_teams(cup, group) > 3) {
+	while (get_amount_of_registered_teams(teams, group) > 3) {
 		printf("The group %c is already complete\n", group);
 		
 		group = get_group(group_names);
 	}
 
-	get_team(cup, group);
+	get_team(teams, false, group);
 }
