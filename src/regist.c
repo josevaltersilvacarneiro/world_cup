6227ba691ec8a5441e78cf1660cf6c19a888f82e
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2023-01-22 17:56:07 -0300

    A bug was fixed: the name of the variable was misspelled

--- src/regist.c
+++ src/regist.c
@@ -9,11 +9,11 @@ regist_team(TEAM teams)
 {
 	char group;
 
-	group = get_group(group_names);
+	group = get_group(groups);
 	while (get_amount_of_registered_teams(teams, group) > 3) {
 		printf("The group %c is already complete\n", group);
 		
-		group = get_group(group_names);
+		group = get_group(groups);
 	}
 
 	get_team(teams, false, group);
