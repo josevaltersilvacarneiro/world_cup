951b2d540007e8ee0c710b0e18e013488739b3a3
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2023-01-26 20:00:53 -0300

    Adding prototypes to functions

--- src/util.h
+++ src/util.h
@@ -2,6 +2,20 @@
 #include "config.h"
 #endif /* config_h */
 
+size_t fact(size_t number);
+void clear_input_buffer(void);
+size_t get_string(char *string, const char *message);
+size_t get_amount(const char *message);
 char get_option(const char options[], unsigned int amount_of_options);
 char get_group_with_registered_teams(TEAM *teams, char groups[]);
+char *add_new_team(TEAM *teams, char group);
+size_t maximum_amount_of_registered_games_group(TEAM *teams, char group);
+size_t maximum_amount_of_registered_games(TEAM *teams, char groups[]);
 unsigned int get_amount_of_registered_teams(TEAM *teams, char group);
+size_t get_amount_of_registered_games(GAME *games, char group);
+
+size_t number_of_teams_registered(TEAM *teams);
+size_t number_of_games_registered(GAME *games);
+
+size_t number_of_teams_to_register(TEAM *teams);
+size_t number_of_games_to_register(GAME *games);
