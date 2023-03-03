7292ae23585898594290ae187556c3cfeafb60a7
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2023-03-03 16:56:39 -0300

    Updating the functions according to the new linked list model

--- src/delete.c
+++ src/delete.c
@@ -6,14 +6,14 @@
 #include "config.h"
 #endif /* config_h */
 
-extern TEAM  *convert_to_team_ptr(TEAM *first_team, const String team_name);
-extern TEAM  *convert_to_before_team_ptr(TEAM *first_team, const String team_name);
+extern TEAM  *convert_to_team_ptr(TEAM **first_team, const String team_name);
+extern TEAM  *convert_to_before_team_ptr(TEAM **first_team, const String team_name);
 
-extern GAME  *find_before_game(GAME *first_game, const TEAM *team_one, const TEAM *team_two);
+extern GAME  *find_before_game(GAME **first_game, const TEAM *team_one, const TEAM *team_two);
 
-extern String get_team(TEAM *first_team, bool is_in);
+extern String get_team(TEAM **first_team, bool is_in);
 
-extern void   unscramble_game(GAME *game);
+extern void   unscramble_game(GAME *game); /* See rank.c */
 
 static void
 real_delete_game(GAME *before_game)
@@ -29,26 +29,45 @@ real_delete_game(GAME *before_game)
 }
 
 void
-delete_team(TEAM *first_team, GAME *first_game)
+delete_team(TEAM **first_team, GAME **first_game)
 {
 	TEAM *before_team;
 	TEAM *team;
 
 	before_team = convert_to_before_team_ptr(first_team, get_team(first_team, true));
-	team	    = before_team->next;
 
-	for (GAME *game_ptr = first_game; game_ptr->next != NULL; game_ptr = game_ptr->next)
-		if (game_ptr->next->team_one == team->next || game_ptr->next->team_two == team->next)
-			real_delete_game(game_ptr); /* before */
+	if (before_team != NULL) {
+		team = before_team->next;
 
-	/* This team isn't last */
-	before_team->next = (team->next != NULL) ? team->next : NULL;
+		if (*first_game != NULL) {
+			GAME *before_game == NULL;
 
-	free(team);
+			for (GAME *game_ptr = *first_game; game_ptr->next != NULL; game_ptr = game_ptr->next) {
+				if (game_ptr->team_one == team || game_ptr->team_two == team) {
+					if (before_game == NULL) {
+						unscramble_game(game);
+						
+						free(*first_game);
+						*first_game = NULL;
+					} else
+						real_delete_game(before_game); /* before */
+				}
+				before_game = game_ptr;
+			} /* end of the for loop */
+		}
+
+		/* This team isn't last */
+		before_team->next = (team->next != NULL) ? team->next : NULL;
+	
+		free(team);
+	} else { /* The team is exactly the head of the list */
+		free(*first_team);
+		*first_team = NULL;
+	}
 }
 
 void
-delete_game(TEAM *first_team, GAME *first_game)
+delete_game(TEAM **first_team, GAME **first_game)
 {
 	TEAM *team_one;
 	TEAM *team_two;
