1d8ec23d2bee33a6e3b912f386d3f10a7bdd6472
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2023-03-04 10:42:14 -0300

    Making it compatible with the new linked list format

--- src/regist.c
+++ src/regist.c
@@ -8,15 +8,15 @@
 
 extern char   groups[];
 
-extern TEAM  *convert_to_team_ptr(const TEAM *teams, const String team_name);
+extern TEAM  *convert_to_team_ptr(const TEAM **first_team, const String team_name);
 
 extern char   get_group(char groups[]);
-extern String get_team(const TEAM *teams, bool is_in);
+extern String get_team(const TEAM **first_team, bool is_in);
 extern size_t get_date();
 extern String get_place();
 
 extern TEAM  *add_team(
-			TEAM *teams,
+			TEAM **first_team,
 			const char group,
 			const String team,
 			const unsigned int pt,
@@ -26,8 +26,8 @@ extern TEAM  *add_team(
 		    );
 
 extern GAME  *add_game(
-			const TEAM *teams,
-			GAME *games,
+			const TEAM **first_team,
+			GAME **first_game,
 			TEAM *team_one,
 			TEAM *team_two,
 			const unsigned short team_one_goals,
@@ -36,35 +36,35 @@ extern GAME  *add_game(
 			String place
 		    );
 
-extern size_t get_amount_of_registered_teams(TEAM *teams, char group);
-extern size_t number_of_teams_registered(TEAM *teams);
-extern size_t number_of_teams_to_register(TEAM *teams);
+extern size_t get_amount_of_registered_teams(TEAM **first_team, char group);
+extern size_t number_of_teams_registered(TEAM **first_team);
+extern size_t number_of_teams_to_register(TEAM **first_team);
 
-extern size_t get_amount_of_registered_games(GAME *games, char group);
-extern size_t number_of_games_registered(GAME *games);
-extern size_t number_of_games_to_register(const TEAM *teams, GAME *games, char groups[]);
-extern size_t maximum_amount_of_registered_games_group(const TEAM *teams, char group);
+extern size_t get_amount_of_registered_games(GAME **first_game, char group);
+extern size_t number_of_games_registered(GAME **first_game);
+extern size_t number_of_games_to_register(const TEAM **first_team, GAME **first_game, char groups[]);
+extern size_t maximum_amount_of_registered_games_group(const TEAM **first_team, char group);
 
 extern size_t get_amount(const char *message);
 
 TEAM
-*regist_team(TEAM *teams)
+*regist_team(TEAM **first_team)
 {
 	char group;
 	String team;
 
 	group = get_group(groups);
-	while (get_amount_of_registered_teams(teams, group) > 3) {
+	while (get_amount_of_registered_teams(first_team, group) > 3) {
 		printf("The group %c is already complete\n", group);
 		group = get_group(groups);
 	}
 
-	team = get_team(teams, false);
-	return add_team(teams, group, team, 0, 0, 0, 0); /* See add.c */
+	team = get_team(first_team, false);
+	return add_team(first_team, group, team, 0, 0, 0, 0); /* See add.c */
 }
 
 GAME
-*regist_game(const TEAM *teams, GAME *games)
+*regist_game(const TEAM **first_team, GAME **first_game)
 {
 	size_t maximum_amount_of_games;
 	
@@ -77,21 +77,23 @@ GAME
 	GAME *last_game;
 
 	rg_game:
-		team_one = convert_to_team_ptr(teams, get_team(teams, true));
+		team_one = convert_to_team_ptr(first_team, get_team(first_team, true));
 		
 		for (;;) {
-			maximum_amount_of_games = maximum_amount_of_registered_games_group(teams, team_one->group);
+			maximum_amount_of_games = maximum_amount_of_registered_games_group(
+					first_team, team_one->group
+				);
 
 			/* ↓ Fundamental Counting Theorem ↓ */
 			if (maximum_amount_of_games == AMOUNT_OF_TEAMS_PER_GROUP * (AMOUNT_OF_TEAMS_PER_GROUP - 1) / 2)
 				puts("All games in this group already were registered");
-			else if (maximum_amount_of_games == get_amount_of_registered_games(games, team_one->group))
+			else if (maximum_amount_of_games == get_amount_of_registered_games(first_game, team_one->group))
 				puts("To register more games in this group, add more teams");
 			else
 				break;
 		}
 	
-	team_two = convert_to_team_ptr(teams, get_team(teams, true));
+	team_two = convert_to_team_ptr(first_team, get_team(first_team, true));
 
 	if (team_one->group != team_two->group) {
 		printf("%s isn't in group %c\n", team_one->name, team_two->group);
@@ -104,8 +106,8 @@ GAME
 	place = get_place();
 
 	return add_game(
-			teams,
-			games,
+			first_team,
+			first_game,
 			team_one,
 			team_two,
 			team_one_goals,
@@ -116,11 +118,11 @@ GAME
 }
 
 TEAM
-*regist_teams(TEAM *teams)
+*regist_teams(TEAM **first_team)
 {
 	unsigned int max_amount, amount_of_teams;
 	
-	max_amount = number_of_teams_to_register(teams);
+	max_amount = number_of_teams_to_register(first_team);
 
 	amount_of_teams = get_amount("Number of teams: ");
 	while (amount_of_teams > max_amount) {
@@ -129,20 +131,20 @@ TEAM
 	}
 	
 	if (amount_of_teams <= 0)
-		return teams;  /* The user gave up on registering more teams */
+		return *first_team;  /* The user gave up on registering more teams */
 
 	while (amount_of_teams-- > 0)
-		teams = regist_team(teams);
+		*first_team = regist_team(first_team);
 
-	return teams;
+	return *first_team;
 }
 
 GAME
-*regist_games(const TEAM *teams, GAME *games)
+*regist_games(const TEAM **first_team, GAME **first_game)
 {
 	size_t max_amount, amount_of_games;
 
-	max_amount = number_of_games_to_register(teams, games, groups);
+	max_amount = number_of_games_to_register(first_team, first_game, groups);
 
 	/*
 	 * As long as amount of games typed by the user is higher
@@ -156,10 +158,10 @@ GAME
 	}
 
 	if (amount_of_games <= 0)
-		return games;  /* The user gave up on registering more games */
+		return *first_game;  /* The user gave up on registering more games */
 
 	while (amount_of_games-- > 0)
-		games = regist_game(teams, games);
+		*first_game = regist_game(first_team, first_game);
 
-	return games;
+	return *first_game;
 }
